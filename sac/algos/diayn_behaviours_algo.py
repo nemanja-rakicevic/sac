e0647f14e3b55a3b8777bfac03d2db75d8819fc5
Nemanja <n.rakicevic@imperial.ac.uk>
2020-05-20 01:43:07 +0100

    added eval_freq; added fixed env start

--- sac/algos/diayn_behaviours_algo.py
+++ sac/algos/diayn_behaviours_algo.py
@@ -19,26 +19,18 @@ import os
 import scipy.stats
 import tensorflow as tf
 
+
+import csv
+import time
+import pickle
+from operator import itemgetter 
+
 import pdb
 
 EPS = 1E-6
 
-# env._wrapped_env.env_id
-# self.environment.bd_metric.restart()
-
-# self.environment.bd_metric.update(info_dict=info_dict)
-
-
-# trial_metric = self.environment.bd_metric.calculate(
-#                                               traj=self.traj_main,
-#                                               traj_aux=self.traj_aux)
-
-# # Initialise behaviour descriptor metric
-# bd_class = ''.join([s.capitalize() for s in metric['type'].split('_')])
-# self.bd_metric = bmet.__dict__[env_class+bd_class](**metric, 
-#                                                 	 **self.env_info)
-
-
+EPISODE_LIMIT = 1000000
+SEED_TASK = 100
 
 
 class DeterministicFixedOptionPolicy(FixedOptionPolicy):
@@ -53,19 +45,110 @@ class DeterministicFixedOptionPolicy(FixedOptionPolicy):
 
 class DIAYN_BD(DIAYN):
 
-    # def __init__(self, *args, **kwargs):
 
+    def __init__(self,
+                 base_kwargs,
+                 env,
+                 policy,
+                 discriminator,
+                 qf,
+                 vf,
+                 pool,
 
-    #     print("\n\n===", args)
-    #     print("---", kwargs)
+                 metric,
+                 env_id,
+                 log_dir,
+                 eval_freq,
 
-    #     super().__init__(*args, **kwargs)
+                 plotter=None,
+                 lr=3E-3,
+                 scale_entropy=1,
+                 discount=0.99,
+                 tau=0.01,
+                 num_skills=20,
+                 save_full_state=False,
+                 find_best_skill_interval=10,
+                 best_skill_n_rollouts=10,
+                 learn_p_z=False,
+                 include_actions=False,
+                 add_p_z=True):
+        """
+            Same as DIAYN just added behaviour descriptor tracking and passing 
+            env and log info.
+        """
 
-    #     # Initialise behaviour descriptor metric
-    #     env_class = env._wrapped_env.env_id.split('Env')[0]
-    #     bd_class = 'GaitGrid'
-    #     self.bd_metric = bmet.__dict__[env_class+bd_class](**metric, 
-    #                                                        **self.env_info)
+        Serializable.quick_init(self, locals())
+        super(SAC, self).__init__(**base_kwargs)
+
+        self._env = env
+        self._policy = policy
+        self._discriminator = discriminator
+        self._qf = qf
+        self._vf = vf
+        self._pool = pool
+        self._plotter = plotter
+
+        self._policy_lr = lr
+        self._discriminator_lr = lr
+        self._qf_lr = lr
+        self._vf_lr = lr
+        self._scale_entropy = scale_entropy
+        self._discount = discount
+        self._tau = tau
+        self._num_skills = num_skills
+        self._p_z = np.full(num_skills, 1.0 / num_skills)
+        self._find_best_skill_interval = find_best_skill_interval
+        self._best_skill_n_rollouts = best_skill_n_rollouts
+        self._learn_p_z = learn_p_z
+        self._save_full_state = save_full_state
+        self._include_actions = include_actions
+        self._add_p_z = add_p_z
+
+        self._Da = self._env.action_space.flat_dim
+        self._Do = self._env.observation_space.flat_dim
+
+        self._training_ops = list()
+
+        self._init_placeholders()
+        self._init_actor_update()
+        self._init_critic_update()
+        self._init_discriminator_update()
+        self._init_target_ops()
+
+        self._sess.run(tf.global_variables_initializer())
+
+        ### Additional params for behaviour tracking ###
+
+        self.dirname = log_dir
+        self.eval_freq = eval_freq
+        self.env_id = env_id
+        logger.set_snapshot_dir('snapshots')
+
+        # Initialise behaviour descriptor metric
+        env_info = env._wrapped_env.env.env_info
+        env_class = ''.join([c.capitalize() for c in env_id.split('_')])
+        bd_class = ''.join([s.capitalize() for s in metric['type'].split('_')])
+        self.bd_metric = bmet.__dict__[env_class+bd_class](**metric, **env_info)
+
+        # Initialise data discovery writer
+        if not os.path.isdir(self.dirname):
+            os.makedirs(self.dirname)
+        filepath = '{}/ref_data_DIAYN_{}.csv'.format(self.dirname, self.env_id)
+        with open(filepath, 'a') as outfile: 
+            writer = csv.DictWriter(outfile, 
+                fieldnames = ["nloop", "niter", "nsmp", "nstep", 
+                              "coverage", "fitness", "outcomes", "ratios"])
+            writer.writeheader()
+
+        # Save the modified arguments
+        metadata_dict = {"experiment": {"controller": {"architecture": [20, 20],
+                                                        "type": "nn_policy"},
+                                         "environment": {"id": env_id},
+                                         "metric": metric,
+                                         "type": "nn_policy"}}
+        filename = '{}/experiment_metadata.json'.format(self.dirname)
+        with open(filename, 'w') as outfile:  
+            json.dump(metadata_dict, outfile, sort_keys=True, indent=4)
 
 
 
@@ -75,17 +158,7 @@ class DIAYN_BD(DIAYN):
         :param env: Environment instance.
         :param policy:  Policy instance.
         :return: None
-        """
-
-        # Initialise behaviour descriptor metric
-        env_class = env._wrapped_env.env_id.split('Env')[0]
-        bd_class = 'ContactGrid' if 'Striker' in env_class else 'GaitGrid'
-        metric = {"type": "contact_grid", "dim": 30} if 'Striker' in env_class \
-                else {"type": "gait_grid", "dim": 10} 
-        env_info = env._wrapped_env.env.env_info
-        self.bd_metric = bmet.__dict__[env_class+bd_class](**metric, 
-                                                           **env_info)
-
+        """      
         self._env = env
         if self._eval_n_episodes > 0:
             if 'Bullet' in env._wrapped_env.env_id \
@@ -97,23 +170,105 @@ class DIAYN_BD(DIAYN):
         self._pool = pool
 
 
-    def sample_skills_to_bd(self, **kwargs):
+    def _save_dataset(self, **task_data_dict):
+        """ Save trial data """
+        if not os.path.isdir(self.dirname):
+            os.makedirs(self.dirname)
+        with open(self.dirname+"/experiment_dataset.dat", "wb") as f:
+            pickle.dump(task_data_dict, f)
 
+
+    def _write_discovery(self, eval_time, unique_bds, unique_outcomes,
+                               n_epoch, n_episodes):
+        """ Save trial output info """
+        n_eval_episodes = n_episodes - self.prev_n_episodes
+        self.prev_n_episodes = n_episodes
+
+        # Log statistics
+        logger.log("(EPOCH {}, EP{}) Evaluaiton of {} skills:"\
+                   "\n\t\t\t\tTime: {};"
+                   "\n\t\t\t\tEpisodes since last eval: {}"
+                   "\n\t\t\t\tTotal behaviours: {}".format(n_epoch, n_episodes,
+                        self._num_skills, eval_time,
+                        n_eval_episodes, len(unique_bds)))
+
+        exploration_data = [0]+[n_epoch]+[n_eval_episodes]+[0] \
+                            + [len(unique_bds)] \
+                            + [max(unique_outcomes[:, 1])] \
+                            + [sum(unique_outcomes[:, 0]==0)] \
+                            + [-1]
+        if not os.path.isdir(self.dirname):
+            os.makedirs(self.dirname)
+        filepath = '{}/ref_data_DIAYN_{}.csv'.format(self.dirname, self.env_id)
+        with open(filepath, 'a') as outfile: 
+            writer = csv.writer(outfile) 
+            writer.writerows([exploration_data])
+
+
+    def sample_skills_to_bd(self, final=False, **kwargs):
+        """
+            Evaluate all the latent skills 
+            and extract the behaviour descriptors
+        """
+        list_traj_main = []
+        list_traj_aux = []
+        list_outcomes = []
+        list_skill_bd = []
+
+        eval_time = time.time()
+                                                                                ### PARALELIZE THIS
         for z in range(self._num_skills):
-            # Make policy  deterministic
+            # Make policy deterministic
             fixed_z_policy = DeterministicFixedOptionPolicy(self._policy, 
                                                             self._num_skills, z)
+            # Evaluate skill
+            self._eval_env._wrapped_env.env.initialize(seed_task=SEED_TASK)
             paths = rollouts(env=self._eval_env, 
                              policy=fixed_z_policy,
                              path_length=self._max_path_length, 
                              n_paths=1,
                              render=False)
+            # Extract trajectory from paths
+            traj_main = paths[0]['env_infos']['position']
+            traj_aux = paths[0]['env_infos']['position_aux']
+            list_traj_main.append(traj_main)
+            list_traj_aux.append(traj_main)
+            # Extract outcomes from paths
+            trial_outcome = self._eval_env._wrapped_env.env.finalize(
+                                                state=paths[0]['last_obs'], 
+                                                rew_list=paths[0]['rewards'],
+                                                traj=traj_main)
+            list_outcomes.append(trial_outcome)
+            # Extract and convert bd from paths
+            self.bd_metric.restart()
+            if self.bd_metric.metric_name == 'contact_grid':
+                [self.bd_metric.update({'contact_objects': idict}) \
+                    for idict in paths[0]['env_infos']['contact_objects']]
+            trial_metric = self.bd_metric.calculate(traj=traj_main,
+                                                    traj_aux=traj_aux)
+            list_skill_bd.append(np.argmax(trial_metric))
 
-            # Convert paths to bd (list)
-            print(paths[0]['env_infos'])
-            
+        eval_time = time.time()-eval_time
 
-            # Save to file so it's loadable, nump epoch, num episodes
+        # Extract unique data
+        unique_bds, unique_idx = np.unique(list_skill_bd, return_index=True)
+        n_bd = len(unique_bds)
+        unique_outcomes = np.array(list_outcomes)[unique_idx]
+        unique_traj_main = itemgetter(*unique_idx)(list_traj_main)
+        unique_traj_aux = itemgetter(*unique_idx)(list_traj_aux)
+
+        # Save to file at exact point: nump epoch, num episodes
+        self._write_discovery(eval_time=eval_time, unique_bds=unique_bds, 
+                              unique_outcomes=unique_outcomes, **kwargs)
+
+        # Save data at the end
+        if final==True:
+            unique_bds_1hot = np.zeros((n_bd, self.bd_metric.metric_size))
+            unique_bds_1hot[np.arange(n_bd), unique_bds] = 1
+            self._save_dataset(**{"outcomes": unique_outcomes, 
+                                  "traj_main": unique_traj_main,
+                                  "traj_aux": unique_traj_aux,
+                                  "metric_bd": unique_bds_1hot})
 
 
 
@@ -123,6 +278,7 @@ class DIAYN_BD(DIAYN):
         self._init_training(env, policy, pool)
 
         with self._sess.as_default():
+            env._wrapped_env.env.initialize(seed_task=SEED_TASK)
             observation = env.reset()
             policy.reset()
             log_p_z_episode = []  # Store log_p_z for this episode
@@ -131,6 +287,7 @@ class DIAYN_BD(DIAYN):
             last_path_return = 0
             max_path_return = -np.inf
             n_episodes = 0
+            self.prev_n_episodes = 0
 
             if self._learn_p_z:
                 log_p_z_list = [deque(maxlen=self._max_path_length) for _ in range(self._num_skills)]
@@ -141,8 +298,6 @@ class DIAYN_BD(DIAYN):
 
             for epoch in gt.timed_for(range(self._n_epochs + 1),
                                       save_itrs=True):
-                logger.push_prefix('Epoch #%d | ' % epoch)
-
 
                 path_length_list = []
                 z = self._sample_z()
@@ -179,6 +334,7 @@ class DIAYN_BD(DIAYN):
 
                     if terminal or path_length >= self._max_path_length:
                         path_length_list.append(path_length)
+                        env._wrapped_env.env.initialize(seed_task=SEED_TASK)
                         observation = env.reset()
                         policy.reset()
                         log_p_z_episode = []
@@ -189,10 +345,18 @@ class DIAYN_BD(DIAYN):
                         path_return = 0
                         n_episodes += 1
 
-                        # EPISORE IS DONE n_episodes
+
+                        # EPOCH IS DONE epoch
+                        if not n_episodes % self.eval_freq:
+                            self.sample_skills_to_bd(final=epoch==self._n_epochs,
+                                                     n_epoch=epoch, 
+                                                     n_episodes=n_episodes)
+
+                            gt.stamp('behaviours')
 
                     else:
                         aug_obs = aug_next_ob
+
                     gt.stamp('sample')
 
                     if self._pool.size >= self._min_pool_size:
@@ -201,6 +365,17 @@ class DIAYN_BD(DIAYN):
                             self._do_training(iteration, batch)
 
                     gt.stamp('train')
+
+
+                    # Terminate after 1000000 episodes
+                    if n_episodes >= EPISODE_LIMIT:
+                        break
+
+
+                else:
+                    continue
+                break
+
 
                 if self._learn_p_z:
                     print('learning p(z)')
@@ -212,36 +387,29 @@ class DIAYN_BD(DIAYN):
                     self._p_z = utils._softmax(log_p_z)
 
 
-                # EPOCH IS DONE epoch
-                self.sample_skills_to_bd(n_episodes=n_episodes, epoch=epoch)
+            logger.push_prefix('Epoch #%d | ' % epoch)
+            self._evaluate(epoch)
 
-                # Epoch
-                self._evaluate(epoch)
+            params = self.get_snapshot(epoch)
+            logger.save_itr_params(epoch, params)
+            times_itrs = gt.get_times().stamps.itrs
 
-                params = self.get_snapshot(epoch)
-                logger.save_itr_params(epoch, params)
-                times_itrs = gt.get_times().stamps.itrs
+            eval_time = times_itrs['eval'][-1] if epoch > 1 else 0
+            total_time = gt.get_times().total
+            logger.record_tabular('time-train', times_itrs['train'][-1])
+            logger.record_tabular('time-eval', eval_time)
+            logger.record_tabular('time-sample', times_itrs['sample'][-1])
+            logger.record_tabular('time-total', total_time)
+            logger.record_tabular('epoch', epoch)
+            logger.record_tabular('episodes', n_episodes)
+            logger.record_tabular('max-path-return', max_path_return)
+            logger.record_tabular('last-path-return', last_path_return)
+            logger.record_tabular('pool-size', self._pool.size)
+            logger.record_tabular('path-length', np.mean(path_length_list))
 
-                eval_time = times_itrs['eval'][-1] if epoch > 1 else 0
-                total_time = gt.get_times().total
-                logger.record_tabular('time-train', times_itrs['train'][-1])
-                logger.record_tabular('time-eval', eval_time)
-                logger.record_tabular('time-sample', times_itrs['sample'][-1])
-                logger.record_tabular('time-total', total_time)
-                logger.record_tabular('epoch', epoch)
-                logger.record_tabular('episodes', n_episodes)
-                logger.record_tabular('max-path-return', max_path_return)
-                logger.record_tabular('last-path-return', last_path_return)
-                logger.record_tabular('pool-size', self._pool.size)
-                logger.record_tabular('path-length', np.mean(path_length_list))
+            logger.dump_tabular(with_prefix=False)
+            logger.pop_prefix()
 
-                logger.dump_tabular(with_prefix=False)
-                logger.pop_prefix()
-
-                gt.stamp('eval')
-
-
-            # EVALUATION
-            self.sample_skills_to_bd()
+            gt.stamp('eval')
 
             env.terminate()
